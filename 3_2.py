16b1caabfed37859009c333e34a2f73516485619
ryu <215513592@qq.com>
2022-01-12 00:08:01 +0800

    1。提交新代码

--- 3_2.py
+++ 3_2.py
@@ -1,4 +1,22 @@
-import numpy as np
 import cv2 as cv
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
+# 加载两张图片
+img1 = cv.imread('1.jpg')
+img2 = cv.imread('2.jpg')
+img3 = img1[0:900, -1000:, :]
+img4 = img2[0:900, -1000:, :]
+i = 0
+j = 1
+while True:
+    e1 = cv.getTickCount()
+    img5 = cv.addWeighted(img3, i, img4, j, 0)
+    cv.imshow('res', img5)
+    cv.waitKey(1000)
+    if i >= 0.99:
+        break
+    i += 0.1
+    j -= 0.1
+    e2 = cv.getTickCount()
+    print((e2-e1)/cv.getTickFrequency())
+cv.destroyAllWindows()
