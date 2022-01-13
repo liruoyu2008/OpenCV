c65d757c8180e9ab3af74fc35cfbee7085acf595
Ryu <215513592@qq.com>
2022-01-13 16:31:40 +0800

    提交add

---
+++ 2_5.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2 as cv
+
+
+def nothing(x):
+    pass
+
+
+# 创建一个黑色的图像，一个窗口
+img = np.zeros((300, 512, 3), np.uint8)
+cv.namedWindow('image')
+# 创建颜色变化的轨迹栏
+cv.createTrackbar('R', 'image', 0, 255, nothing)
+cv.createTrackbar('G', 'image', 0, 255, nothing)
+cv.createTrackbar('B', 'image', 0, 255, nothing)
+# 为 ON/OFF 功能创建开关
+switch = '0 : OFF \n1 : ON'
+cv.createTrackbar(switch, 'image', 0, 1, nothing)
+while (1):
+    cv.imshow('image', img)
+    k = cv.waitKey(1) & 0xFF
+    if k == 27:
+        break
+    # 得到四条轨迹的当前位置
+    r = cv.getTrackbarPos('R', 'image')
+    g = cv.getTrackbarPos('G', 'image')
+    b = cv.getTrackbarPos('B', 'image')
+    s = cv.getTrackbarPos(switch, 'image')
+    if s == 0:
+        img[:] = 0
+    else:
+        # img[:] = [b,g,r]
+        cv.putText(img, 'OpenCV', (50, 50), cv.FONT_HERSHEY_SIMPLEX,
+                   4, (b, g, r), 2, cv.LINE_AA)
+cv.destroyAllWindows()
