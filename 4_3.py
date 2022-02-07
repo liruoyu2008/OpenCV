310bb085c7f61db963865169cd158094fe73ba20
Ryu <215513592@qq.com>
2022-02-07 18:35:06 +0800

    1.提交各滤波器代码

---
+++ 4_3.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+
+def func1():
+    '''缩放'''
+    import cv2 as cv
+    import numpy as np
+    from matplotlib import pyplot as plt
+    img = cv.imread('1.jpg',0)
+    ret,thresh1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+    ret,thresh2 = cv.threshold(img,127,255,cv.THRESH_BINARY_INV)
+    ret,thresh3 = cv.threshold(img,127,255,cv.THRESH_TRUNC)
+    ret,thresh4 = cv.threshold(img,127,255,cv.THRESH_TOZERO)
+    ret,thresh5 = cv.threshold(img,127,255,cv.THRESH_TOZERO_INV)
+    titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+    images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+    for i in range(6):
+        plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
+        plt.title(titles[i])
+        plt.xticks([]),plt.yticks([])
+    plt.show()
+
+
+def func2():
+    pass
+
+
+if __name__=='__main__':
+    func1()
---
+++ 4_4.py
@@ -0,0 +1,100 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+
+def some_filter():
+    img = plt.imread('1.jpg')
+    # img = cv.cvtColor(cv.imread("1.jpg"), cv.COLOR_BGR2RGB)
+
+    # 均值滤波
+    mean_kernel = np.array((
+        [0.0625, 0.125, 0.0625],
+        [0.125, 0.25, 0.125],
+        [0.0625, 0.125, 0.0625]), dtype=np.float32)
+    mean_dst = cv.filter2D(img, -1, mean_kernel)
+
+    # sobel_buttom滤波
+    sobel1_kernel = np.array((
+        [-1, -2, -1],
+        [0, 0, 0],
+        [1, 2, 1]), dtype=np.float32)
+    sobel1_dst = cv.filter2D(img, -1, sobel1_kernel)
+
+    # sobel_left滤波
+    sobel2_kernel = np.array((
+        [1, 0, -1],
+        [2, 0, -2],
+        [1, 0, -1]), dtype=np.float32)
+    sobel2_dst = cv.filter2D(img, -1, sobel2_kernel)
+
+    # emboss浮雕滤波
+    emboss_kernel = np.array((
+        [-2, -1, 0],
+        [-1, 1, 1],
+        [0, 1, 2]), dtype=np.float32)
+    emboss_dst = cv.filter2D(img, -1, emboss_kernel)
+
+    # outline描边（大纲、轮廓）滤波
+    outline_kernel = np.array((
+        [-1, -1, -1],
+        [-1, 8, -1],
+        [-1, -1, -1]), dtype=np.float32)
+    outline_dst = cv.filter2D(img, -1, outline_kernel)
+
+    # sharpen锐化滤波
+    sharpen_kernel = np.array((
+        [0, -1, 0],
+        [-1, 5, -1],
+        [0, -1, 0]), dtype=np.float32)
+    sharpen_dst = cv.filter2D(img, -1, sharpen_kernel)
+
+    # laplacian拉普拉斯滤波
+    laplacian_kernel = np.array((
+        [0, 2, 0],
+        [2, -8, 2],
+        [0, 2, 0]), dtype=np.float32)
+    laplacian_dst = cv.filter2D(img, -1, laplacian_kernel)
+
+    # identity分身滤波
+    identity_kernel = np.array((
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]), dtype=np.float32)
+    identity_dst = cv.filter2D(img, -1, identity_kernel)
+
+    # gaussian_blur高斯模糊
+    # gaussian_dst = cv.GaussianBlur(img, (5, 5), 0)
+    gaussian_blur_kernel = cv.getGaussianKernel(19, 0)
+    gaussian_dst = cv.filter2D(img, -1, gaussian_blur_kernel)
+
+    # bilateral双边滤波
+    bilateral_dst = cv.bilateralFilter(img, 9, 75, 75)
+
+    plt.subplot(341), plt.imshow(img), plt.title('original')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(342), plt.imshow(mean_dst), plt.title('mean')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(343), plt.imshow(sobel1_dst), plt.title('sobel_buttom')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(344), plt.imshow(sobel2_dst), plt.title('sobel_left')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(345), plt.imshow(emboss_dst), plt.title('emboss')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(346), plt.imshow(outline_dst), plt.title('outline')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(346), plt.imshow(sharpen_dst), plt.title('sharpen')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(347), plt.imshow(laplacian_dst), plt.title('laplacian')
+    plt.xticks([]), plt.yticks([])
+    # plt.subplot(348), plt.imshow(identity_dst), plt.title('identity')
+    # plt.xticks([]), plt.yticks([])
+    plt.subplot(348), plt.imshow(gaussian_dst), plt.title('gaussian_blur')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(349), plt.imshow(bilateral_dst), plt.title('bilateral')
+    plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
+if __name__ == '__main__':
+    some_filter()
