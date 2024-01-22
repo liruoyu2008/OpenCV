2f0efe7e486b5acf783ce3200c56e67616449155
Ryu <215513592@qq.com>
2024-01-22 17:24:45 +0800

    学习形态学

--- 4_5.py
+++ 4_5.py
@@ -3,54 +3,62 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+from cv2.typing import MatLike
 
-img = plt.imread('1.jpg')
+img1 = cv.imread('./images/4.png', cv.IMREAD_GRAYSCALE)
+img2 = cv.imread('./images/5.png', cv.IMREAD_GRAYSCALE)
 kernel = np.ones((5, 5), np.uint8)
 
 
-def erode():
+def raw(img: MatLike, *args) -> MatLike:
+    '''返回原图'''
+    return img
+
+
+def erode(img: MatLike, kernel: MatLike) -> MatLike:
     '''腐蚀'''
     return cv.erode(img, kernel, iterations=1)
 
 
-def dilate():
+def dilate(img: MatLike, kernel: MatLike) -> MatLike:
     '''膨胀'''
     return cv.dilate(img, kernel, iterations=1)
 
 
-def open():
-    '''开运算（先腐蚀后膨胀，消除白点、噪声）'''
+def open(img: MatLike, kernel: MatLike) -> MatLike:
+    '''开运算（先腐蚀后膨胀，消除高频白噪声即白点）'''
     return cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
 
 
-def close():
-    '''开运算（先腐蚀后膨胀，消除白点、噪声）'''
+def close(img: MatLike, kernel: MatLike) -> MatLike:
+    '''闭运算（先膨胀后腐蚀，消除高频黑噪声即黑斑）'''
     return cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
 
 
-def grad():
-    '''形态学梯度'''
+def grad(img: MatLike, kernel: MatLike) -> MatLike:
+    '''形态学梯度（膨胀和腐蚀之差，凸显轮廓和噪点）'''
     return cv.morphologyEx(img, cv.MORPH_GRADIENT, kernel)
 
 
-def top_hat():
-    '''顶帽运算'''
+def top_hat(img: MatLike, kernel: MatLike) -> MatLike:
+    '''顶帽运算（开运算与原图差,突出高频白噪声）'''
     return cv.morphologyEx(img, cv.MORPH_TOPHAT, kernel)
 
 
-def black_hat():
-    '''黑帽运算'''
+def black_hat(img: MatLike, kernel: MatLike) -> MatLike:
+    '''黑帽运算（闭运算与原图差，突出高频黑噪声）'''
     return cv.morphologyEx(img, cv.MORPH_BLACKHAT, kernel)
 
 
 if __name__ == '__main__':
-    plt.subplot(331), plt.imshow(img), plt.title('origin')
-    plt.subplot(332), plt.imshow(erode()), plt.title('erode')
-    plt.subplot(333), plt.imshow(dilate()), plt.title('dilate')
-    plt.subplot(334), plt.imshow(open()), plt.title('open')
-    plt.subplot(335), plt.imshow(close()), plt.title('close')
-    plt.subplot(336), plt.imshow(grad()), plt.title('grad')
-    plt.subplot(337), plt.imshow(top_hat()), plt.title('top_hat')
-    plt.subplot(338), plt.imshow(black_hat()), plt.title('top_hat')
-
+    imgs = [img1, img2]
+    funcs = [raw, erode, dilate, open, close, grad, top_hat, black_hat]
+    index = 1
+    for img in imgs:
+        for func in funcs:
+            plt.subplot(4, 4, index)
+            plt.imshow(cv.cvtColor(func(img, kernel), cv.COLOR_BGR2RGB))
+            plt.title(func.__name__, fontsize=6)
+            plt.axis("off")
+            index += 1
     plt.show()
